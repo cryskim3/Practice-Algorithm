a985a8693817dd19f4aa1433a6b5a4a85725c96a
cryskim3 <71306184+cryskim3@users.noreply.github.com>
2021-11-24 08:21:38 -0800

    Update valid-parentheses.js

--- valid-parentheses/valid-parentheses.js
+++ valid-parentheses/valid-parentheses.js
@@ -21,3 +21,27 @@ var isValid = function(s) {
     
     return !stack.length;
 };
+
+
+//Using Switch
+// var isValid = function(s) {   
+//     const stack = [];
+    
+//     for (let i = 0 ; i < s.length ; i++) {
+//         let c = s.charAt(i);
+//         switch(c) {
+//             case '(': stack.push(')');
+//                 break;
+//             case '[': stack.push(']');
+//                 break;
+//             case '{': stack.push('}');
+//                 break;
+//             default:
+//                 if (c !== stack.pop()) {
+//                     return false;
+//                 }
+//         }
+//     }
+    
+//     return stack.length === 0;
+// };
