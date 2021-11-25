c79e667d37c414e84bb9169133714765c1163060
cryskim3 <71306184+cryskim3@users.noreply.github.com>
2021-11-25 14:20:00 -0800

    Time: 68 ms (91.11%), Space: 39.8 MB (59.04%) - LeetHub

---
+++ search-insert-position/search-insert-position.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+var searchInsert = function(nums, target) {
+
+    let low = 0;
+    let high = nums.length;
+    
+    while(low < high){
+        let middle = low + Math.floor((high-low)/2);
+        
+        if (target > nums[middle]) {
+            low = middle + 1;
+        } else {
+            high = middle;
+        }
+    }
+    return low;
+    
+};
+
+
+// var searchInsert = function(nums, target) {
+
+//     for(let i=0; i<nums.length; i++){
+//         if(target <=nums[i]){
+//             return i;
+//         }
+//     }
+//     return i;
+// };
+
+//https://leetcode.com/problems/search-insert-position/discuss/423166/Binary-Search-101
