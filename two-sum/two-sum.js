9bd874df49391de1603854b334997d68b5cafa28
cryskim3 <skim552@my.bcit.ca>
2021-11-26 08:40:25 -0800

    Time: 68 ms (91.11%), Space: 39.8 MB (59.04%) - LeetHub

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -4,7 +4,6 @@
  * @return {number[]}
  */
 
-
 // Single "For loop"
 const twoSum = (nums, target) => {
   const map = {};
@@ -22,20 +21,17 @@ const twoSum = (nums, target) => {
   return null;
 };
 
-
-
 // double "For loop"
 
 // var twoSum = function(nums, target) {
-    
+
 //     for(let i=0; i<nums.length; i++){
-       
+
 //         for(let j=i+1; j<nums.length; j++){
-            
+
 //              if((nums[i] + nums[j]) === target)
-            
+
 //         return [i, j];
 //         }
 //     }
 // };
-
