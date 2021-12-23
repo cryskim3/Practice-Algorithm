ef5599aaea5872e14734296e909feb7990fee041
cryskim3 <71306184+cryskim3@users.noreply.github.com>
2021-12-23 08:17:08 -0800

    Time: 72 ms (94.67%), Space: 41.7 MB (71.31%) - LeetHub

--- majority-element/majority-element.js
+++ majority-element/majority-element.js
@@ -4,20 +4,42 @@
  */
 
 
-var majorityElement = function(nums) {
+// var majorityElement = function(nums) {
     
-    let hash = {}
+//     let hash = {}
     
-    for(let i = 0; i<nums.length; i++){
+//     for(let i = 0; i<nums.length; i++){
         
-        if(nums[i] in hash){
-            hash[nums[i]] = hash[nums[i]]+1;
-        }else hash[nums[i]] = 1;
+//         if(nums[i] in hash){
+//             hash[nums[i]] = hash[nums[i]]+1;
+//         }else hash[nums[i]] = 1;
 
-        if(hash[nums[i]] > nums.length/2)
-            return nums[i]
-    } 
+//         if(hash[nums[i]] > nums.length/2)
+//             return nums[i]
+//     } 
+// };
 
+
+var majorityElement = function (nums) {
+  let hash = {};
+  let count = 0;
+  let ans = null;
+
+  for (let i = 0; i < nums.length; i++) {
+    let n = nums[i];
+
+    if (n in hash) {
+      hash[n] = hash[n] + 1;
+    } else hash[n] = 1;
+      
+    if (count < hash[n]) {
+      ans = n;
+      count = hash[n];
+    }
+  }
+  return ans;
 };
+
+
 
 
