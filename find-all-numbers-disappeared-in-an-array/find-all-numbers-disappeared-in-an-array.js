19c175e3112e524528a3ef71ed106c01aafea2c6
cryskim3 <71306184+cryskim3@users.noreply.github.com>
2021-12-31 18:43:58 -0800

    Time: 116 ms (70.76%), Space: 46.4 MB (98.71%) - LeetHub

--- find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
+++ find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -3,9 +3,22 @@
  * @return {number[]}
  */
 
+var findDisappearedNumbers = function(nums) {
+    let res = [];
+    for (let i = 0; i < nums.length; i++) {
+        let num = Math.abs(nums[i]);
+        let idx = num-1;
+        nums[idx] = Math.abs(nums[idx]) * -1;
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > 0) res.push(i+1);
+    }
+    return res;
+};
 
 
-//Time complexity is not O(n^2)
+
+//Time complexity is O(n^2)
 var findDisappearedNumbers = function(nums) {
    
     let missingNumber = [];
